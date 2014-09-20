c7bad4d601282c851fe29fbe6ec3cfb8a2379f17
lugakorrav <lugakorrav@yandex.ru>
2014-09-20 12:27:13 +0400

    new

--- Zadanie_2_var_4/ConsoleApplication6/source.cpp
+++ Zadanie_2_var_4/ConsoleApplication6/source.cpp
@@ -6,11 +6,11 @@ int main()
 	double N;
 	int k,S=0,i=0,p=0;
 	setlocale(LC_ALL, "Russian");
-	cout<<"Введите номер"<<endl;
+	cout<<"Enter the number"<<endl;
 	cin>>N;
 	if((cin.fail())||(N<1)||(((int)(N*10)%10!=0)))
 	{
-		cout<<"Введено некорректное значение"<<endl;
+		cout<<"entered an incorrect number"<<endl;
 	}
 	else
 	{
@@ -28,7 +28,7 @@ int main()
 	    }
 	    k=(p*p)/(pow(10,S-N));
 	    k=k%10;
-	    cout<<"На "<<N<<" месте цифра "<<k<<"."<<endl;
+	    cout<<"The "<<N<<" number is "<<k<<"."<<endl;
 	    return 0;
     }
 }
