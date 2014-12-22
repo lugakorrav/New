6f0197a71770dbd4d3566aad3ed2d6eb2eaa0624
lugakorrav <lugakorrav@yandex.ru>
2014-12-22 17:05:12 +0400

    Update main.cpp

--- Zadanie_5_Beta/main.cpp
+++ Zadanie_5_Beta/main.cpp
@@ -7,12 +7,17 @@ int main()
 {
 	setlocale(LC_ALL, "rus");
 	queue q;
-	cout<<"Введите 3 элемента очереди"<<endl;
-	cin>>q>>q>>q;
-	cout<<"Извлечение первых двух элементов: "<<endl;
-	cout<<q<<" "<<q;
-	cout<<"Текущий размер очереди: "<<endl;
-	q.printsize();
-	system("pause");
+	string str;
+	const int N = 3;
+	int M=0;
+	cout<<"Введите "<<N<<" элементов очереди"<<endl;
+	for (int i=0; i<N; i++)
+		cin>>q;
+	cout<<"Введите число элементов для извлечения"<<endl;
+	cin>>M;
+	cout<<"Извлечение первых "<<M<<" элементов: "<<endl;
+	for (int i=0; i<M; i++)
+	cout<<q<<" "<<endl;
+	cout<<"Текущий размер очереди: "<<q.queuesize()<<endl;
 	return 0;
 }
