226a005b5a0541dded84b6fd2ea993e362014118
lugakorrav <lugakorrav@yandex.ru>
2015-04-08 00:27:50 +0400

    Update main.cpp

--- Set/main.cpp
+++ Set/main.cpp
@@ -1,15 +1,16 @@
 #include "MySet.h"
-
+#include <string>
 using namespace std;
 
-int main()
+void foo1()
 {
 	int arr[3];
 	int check = 321;
 	arr[0] = 123; arr[1] = 456; arr[2] = 789;
-	MySet<int> S(2);
+	MySet<int> S;
 	S.add(arr[0]);
 	S.add(arr[1]);
+	S.add(arr[2]);
 	S.add(arr[2]);
 	S.show();
 	cout << endl << S.size() << endl;
@@ -17,6 +18,31 @@ int main()
 	cout << boolalpha << S.contains(check) << endl;
 	S.remove(arr[1]);
 	S.show();
+	cout << endl << S.size() << endl << endl;
+};
+
+void foo2()
+{
+	string arr[3];
+	string check = "321";
+	arr[0] = "123"; arr[1] = "456"; arr[2] = "789";
+	MySet<string> S;
+	S.add(arr[0]);
+	S.add(arr[1]);
+	S.add(arr[2]);
+	S.add(arr[2]);
+	S.show();
 	cout << endl << S.size() << endl;
+	cout << boolalpha << S.contains(arr[1]) << endl;
+	cout << boolalpha << S.contains(check) << endl;
+	S.remove(arr[1]);
+	S.show();
+	cout << endl << S.size() << endl << endl;
+};
+
+int main()
+{
+	foo1();
+	foo2();
 	return 0;
 }
