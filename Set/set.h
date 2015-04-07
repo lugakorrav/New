3eea886412187ae542bfd8f711eae4b70719e271
lugakorrav <lugakorrav@yandex.ru>
2015-04-08 00:46:27 +0400

    Update set.h

--- Set/set.h
+++ Set/set.h
@@ -1,20 +1,8 @@
 #pragma once
 #include <iostream>
+#include "interfaces.h"
 
 using namespace std;
-
-template <class T> class ISet
-{
-public:
-	// добавить элемент
-	virtual void add(const T& elem) = 0;
-	// удалить элемент
-	virtual void remove(const T& elem) = 0;
-	// содержится ли элемент
-	virtual bool contains(const T& elem) const = 0;
-	// узнать размер множества
-	virtual int size() const = 0;
-};
 
 template <class T> class MySet : public ISet < T >
 {
