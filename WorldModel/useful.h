2be387b1cb9ca88f134489d653356f7442a95121
lugakorrav <lugakorrav@yandex.ru>
2015-04-15 20:47:32 +0400

    Update useful.h

--- WorldModel/useful.h
+++ WorldModel/useful.h
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <Windows.h>
 #include <WinUser.h>
+#include <conio.h>
 #define KEY_N 78
 #define KEY_L 76
 #define KEY_S 83
-#define KEY_SPACE 32
+#define KEY_Z 90
 
 int random(int, int);
