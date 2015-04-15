af0c98e6df6dcd49387965fb6b14ba603e70ba3f
lugakorrav <lugakorrav@yandex.ru>
2015-04-16 01:48:39 +0400

    Update useful.h

--- WorldModel/useful.h
+++ WorldModel/useful.h
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <Windows.h>
-#include <WinUser.h>
 #include <conio.h>
-#define KEY_N 78
-#define KEY_L 76
-#define KEY_S 83
-#define KEY_Z 90
 
 int random(int, int);
+
+bool Pressed(char key);
