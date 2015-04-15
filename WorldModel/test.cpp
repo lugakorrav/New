86c41d75d2723870c512c754c935731de0ebde63
lugakorrav <lugakorrav@yandex.ru>
2015-04-15 20:14:30 +0400

    Create test.cpp

---
+++ WorldModel/test.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <time.h>
+
+#include "useful.h"
+#include "world.h"
+
+int main()
+{
+	//	settings
+	setlocale(LC_ALL, "russian");
+	srand((int)time(NULL));
+	World* w = 0;
+
+	system("pause");
+	if (GetKeyState(KEY_N))
+	{
+		int x, y;
+		std::cout << "Enter the size of the world\n";
+		std::cin >> x >> y;
+		w = new World(x, y);
+	}
+	else
+	if (GetKeyState(KEY_L))
+	{
+		char* c = new char[200];
+		std::cout << "Enter the load file directory\n";
+		std::cin.getline(c, 200);
+		w = new World(c);
+	};
+	if (!w)
+		return 0;
+
+	while (1)
+	{
+		if (GetKeyState(KEY_SPACE))
+		{
+			system("pause");
+			if (GetKeyState(KEY_S))
+			{
+				char* c = new char[200];
+				std::cout << "Enter the save file directory\n";
+				std::cin.getline(c, 200);
+				w->SaveWorld(c);
+			};
+		};
+		w->PerformStep();
+		w->View();
+	};
+
+	system("pause");
+	return true;
+}
