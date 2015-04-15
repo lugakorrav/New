955e13ed21978abf6ae289794514653857bf7e2c
lugakorrav <lugakorrav@yandex.ru>
2015-04-16 03:32:02 +0400

    Update useful.h

--- WorldModel/useful.h
+++ WorldModel/useful.h
@@ -1,6 +1,16 @@
-#include <iostream>
-#include <conio.h>
+#include "useful.h"
 
-int random(int, int);
+int random(int min, int max)
+{
+	return (min + rand() % (max - min + 1));
+}
 
-bool Pressed(char key);
+bool Pressed(char key)
+{
+	if (_kbhit())
+	{
+		char c = _getch();
+		return (c == key || c == key + 32);
+	};
+	return false;
+};
