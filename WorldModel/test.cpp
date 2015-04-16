52b066ce305b84b390f10099ab76f4e9e0db580d
lugakorrav <lugakorrav@yandex.ru>
2015-04-16 18:04:34 +0400

    Update test.cpp

--- WorldModel/test.cpp
+++ WorldModel/test.cpp
@@ -12,39 +12,71 @@ int main()
 	srand((int)time(NULL));
 	World* w = 0;
 
-	std::cout << "N - создать новое поле; L - загрузить из файла\n";
+	std::cout << "1 - создать новое поле; 2 - загрузить из файла\n";
 	char c = _getch();
-	if (c == 'N' || c == 'n')
+	switch (c)
+	{
+	case '1':
 	{
 		int x, y;
 		std::cout << "Введите размеры поля\n";
 		std::cin >> x >> y;
 		w = new World(x, y);
-	}
-	else
-		if (c == 'L' || c == 'l')
-		{
-			char* c = new char[200];
-			std::cout << "Введите имя файла для чтения\n";
-			std::cin.getline(c, 200);
-			w = new World(c);
-			delete[] c;
-		};
+		break;
+	};
+	case '2':
+	{
+		char* c = new char[200];
+		std::cout << "Введите имя файла для чтения\n";
+		std::cin.getline(c, 200);
+		w = new World(c);
+		delete[] c;
+		break;
+	};
+	};
 	if (!w)
 		return 0;
-	std::cout << "S - сохранить\n";
+	std::cout << "1 - сохранить; 2 - пауза; Esc - выход\n";
 	system("pause");
 	while (1)
 	{
-		if (Pressed('S'))
+		if (_kbhit())
 		{
-			char* c = new char[200];
-			std::cout << "Введите имя файла для записи\n";
-			std::cin.get();
-			std::cin.getline(c, 200);
-			w->SaveWorld(c);
-			delete[] c;
-			system("pause");
+			char buf = _getch();
+			switch (buf)
+			{
+			case '1':
+			{
+				char* c = new char[200];
+				std::cout << "Введите имя файла для записи\n";
+				std::cin.get();
+				std::cin.getline(c, 200);
+				w->SaveWorld(c);
+				delete[] c;
+				std::cout << "Сохранено\n";
+				system("pause");
+				break;
+			};
+			case '2':
+			{
+				std::cout << "1 - сохранить\n";
+				char buf = _getch();
+				if (buf == '1')
+				{
+					char* c = new char[200];
+					std::cout << "Введите имя файла для записи\n";
+					std::cin.get();
+					std::cin.getline(c, 200);
+					w->SaveWorld(c);
+					delete[] c;
+					std::cout << "Сохранено\n";
+					system("pause");
+				};
+				break;
+			};
+			case 27:
+				return 0;
+			};
 		};
 		w->PerformStep();
 		w->View();
